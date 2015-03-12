15eeeb3d19f7e622f2549c0144db668a888eb1bb
Jason Dusek <jason.dusek@gmail.com>
2015-03-12 12:11:11 -0700

    Add valid test data transactionally.

--- fb/data.sql
+++ fb/data.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 INSERT INTO consumer (id, full_name) VALUES
   ('606fa027-a577-4018-952e-3c8469372829', 'Curly'),
   ('f3411edc-e1d0-452a-bc19-b42c0d5a0e36', 'Larry'),
@@ -9,3 +11,5 @@ INSERT INTO friendship (first, second) VALUES
    '05ed1b59-090e-40af-8f0e-68a1129b55b4'),
   ('05ed1b59-090e-40af-8f0e-68a1129b55b4',
    'f3411edc-e1d0-452a-bc19-b42c0d5a0e36');
+
+END;
